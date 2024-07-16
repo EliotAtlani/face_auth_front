9635cfee46786c7569b60690aeae050edd79441e
Atlani Eliot <eliot.atlani@student-cs.fr>
2024-07-16 17:40:13 +1000

    Tried something

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,6 @@
-import path from "path";
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
 
 export default defineConfig({
-  base: "",
   plugins: [react()],
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
-    },
-  },
 });
