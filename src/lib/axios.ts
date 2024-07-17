a9191535a0a4f63c430defb3eb279eba055244e0
Atlani Eliot <eliot.atlani@student-cs.fr>
2024-07-17 10:49:06 +1000

    Fix router

--- src/lib/axios.ts
+++ src/lib/axios.ts
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:8000/",
+  baseURL: "http://ec2-3-93-167-67.compute-1.amazonaws.com/",
+  // baseURL: "http://localhost:8000/",
 });
 
 export default api;
