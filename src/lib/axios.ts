9eaf00e0925205910d0b3df6bc9e0893a71e65a1
Atlani Eliot <eliot.atlani@student-cs.fr>
2024-07-17 11:05:28 +1000

    Delete env

--- src/lib/axios.ts
+++ src/lib/axios.ts
@@ -1,8 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://ec2-3-93-167-67.compute-1.amazonaws.com/",
-  // baseURL: "http://localhost:8000/",
+  baseURL: import.meta.env.VITE_API_URL,
 });
 
 export default api;
